58518c5b5941e86387024cd256a4f4bf20ddcd57
Johntor12 <arkanantafirjatullahariza@mail.ugm.ac.id>
2025-04-23 07:51:50 +0700

    feat: add Navbar and update landingpage

--- src/components/LandingPage/OneClick.tsx
+++ src/components/LandingPage/OneClick.tsx
@@ -1,8 +1,46 @@
 const OneClick = () => {
   return (
-    <div className="flex flex-col justify-items-center w-full bg-red-200 font-jakarta font-bold text-[1.806vw]">
-      <div className="flex flex-col items-center">
-        <div className="">Just &quot;One Click&quot;</div>
+    <div className="flex flex-col gap-[2.667vw] justify-center items-center w-full font-jakarta">
+      <div className="flex flex-col gap-[0.667vw] items-center">
+        <h6 className="text-[#420092] font-bold text-[1.806vw]">
+          Just &quot;One Click&quot;
+        </h6>
+        <p className="text-[1.111vw] text-gray-700">
+          Instant RWA verification, ready for minting in a single click.
+        </p>
+        <div className="bg-[#D9D9D9] w-[60vw] aspect-[864/307]"></div>
+      </div>
+      <div className="grid-cols-4 w-[75vw] flex flex-row text-[1.25vw] text-[#420092] px-[2vw]">
+        <div className="col-span-1 flex flex-col items-center gap-y-[1.667vw] w-full">
+          <div className="w-[3.194vw] aspect-square rounded-full bg-transparent border-[#712DC5] border-2 flex justify-center items-center">
+            1
+          </div>
+          <p className="font-jakarta font-bold ">Login</p>
+        </div>
+        <div className="col-span-1 flex flex-col items-center gap-y-[1.667vw] w-full">
+          <div className="w-[3.194vw] aspect-square rounded-full bg-transparent border-[#712DC5] border-2 flex justify-center items-center">
+            2
+          </div>
+          <p className="font-jakarta font-bold ">ExploreRWA</p>
+        </div>
+        <div className="col-span-1 flex flex-col items-center gap-y-[1.667vw] w-full">
+          <div className="w-[3.194vw] aspect-square rounded-full bg-transparent border-[#712DC5] border-2 flex justify-center items-center">
+            3
+          </div>
+          <p className="font-jakarta font-bold ">Deploy</p>
+        </div>
+        <div className="col-span-1 flex flex-col items-center gap-y-[1.667vw] w-full">
+          <div className="w-[3.194vw] aspect-square rounded-full bg-transparent border-[#712DC5] border-2 flex justify-center items-center">
+            4
+          </div>
+          <p className="font-jakarta font-bold ">Verification</p>
+        </div>
+        <div className="col-span-1 flex flex-col items-center gap-y-[1.667vw] w-full">
+          <div className="w-[3.194vw] aspect-square rounded-full bg-transparent border-[#712DC5] border-2 flex justify-center items-center">
+            5
+          </div>
+          <p className="font-jakarta font-bold ">Minting</p>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import Logo from "../assets/Logo_Veristable.png";
+import Image from "next/image";
+import Button from "./Button";
+
+const Navbar = () => {
+  return (
+    <div className="relative">
+      <div className="absolute flex flex-row w-full h-[4.667vw] justify-center items-center font-jakarta">
+        <div className="flex flex-row justify-between items-center w-[90%] h-10">
+          <div className="flex flex-row items-center h-full">
+            <Image
+              src={Logo}
+              width={40}
+              className="aspect-square"
+              alt="Logo Veristable"
+            />
+            <p className="font-jakarta text-[1.25vw] font-bold">Veristable</p>
+          </div>
+          <div className="grid grid-cols-9 justify-items-end  items-center h-full gap-[2.667vw]">
+            <p className="col-span-2 font-jakarta text-[1.111vw]">ExploreRWA</p>
+            <p className="col-span-2 font-jakarta text-[1.111vw]">MyRWA</p>
+            <p className="col-span-2 font-jakarta text-[1.111vw]">Dashboard</p>
+            <Button
+              text="Login"
+              customClass="col-span-3 text-[1.111vw] font-jakarta rounded-[0.694vw] h-10"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
