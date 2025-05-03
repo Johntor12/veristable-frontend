a0d11903d4d0c9d3335e7a9d2850fd3a637768f4
askar full belajar <108921033+askar-ef@users.noreply.github.com>
2025-05-04 01:50:48 +0700

    minor fix

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,7 +6,7 @@ import RegisteredOperators from "@/components/MyRWA/RegisteredOperators";
 export default function DashboardPage() {
   return (
     <div className="min-h-screen bg-white pt-[6vw] px-4 lg:px-0 font-jakarta">
-      <section className="w-[90%] mx-auto py-12 flex flex-col justify-center items-center gap-[5vw]">
+      <section className="w-[90%] mx-auto py-12 flex flex-col justify-center items-center gap-[3vw]">
         <HeroDashboard />
         <TotalAssetsSection />
         <ReservesChart />
